910e7c9717e126a21d000f1b067424f1d72eb397
ebf2027 <ebf2027@gmail.com>
2026-04-10 10:35:12 -0300

    SEO: Ajuste fino TTI e TBT mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,9 +97,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* Google Tag (gtag.js) - Movido para body para obedecer o padrão Next */}
         <Script 
           src="https://www.googletagmanager.com/gtag/js?id=G-9VPVJCNYHH" 
-          strategy="afterInteractive" 
+          strategy="lazyOnload" 
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="lazyOnload">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
